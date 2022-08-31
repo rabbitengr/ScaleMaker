d1cb967152e3e15d66204ab4edc3d5b815d00162
rabbitengr <davenunez@live.com>
2022-08-31 16:20:54 -0700

    Save png

--- ScaleMaker/Form1.Designer.cs
+++ ScaleMaker/Form1.Designer.cs
@@ -30,37 +30,57 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.picturePreview = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picturePreview)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1101, 919);
+            this.button1.Location = new System.Drawing.Point(1067, 913);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.Size = new System.Drawing.Size(163, 34);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Generate";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // picturePreview
             // 
-            this.picturePreview.Location = new System.Drawing.Point(433, 12);
+            this.picturePreview.Location = new System.Drawing.Point(430, 22);
             this.picturePreview.Name = "picturePreview";
             this.picturePreview.Size = new System.Drawing.Size(800, 800);
             this.picturePreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picturePreview.TabIndex = 1;
             this.picturePreview.TabStop = false;
             // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "png";
+            this.saveFileDialog1.Filter = "PNG files|*.png|All files|*.*";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1067, 828);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "save as PNG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1259, 989);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.picturePreview);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picturePreview)).EndInit();
             this.ResumeLayout(false);
 
@@ -70,5 +90,7 @@
 
         private Button button1;
         private PictureBox picturePreview;
+        private SaveFileDialog saveFileDialog1;
+        private Button button2;
     }
 }
--- ScaleMaker/Form1.cs
+++ ScaleMaker/Form1.cs
@@ -2,6 +2,7 @@ using System.Drawing;
 using static System.Windows.Forms.DataFormats;
 using System.IO;
 using System.Drawing.Drawing2D;
+using Microsoft.Win32;
 
 namespace ScaleMaker
 {
@@ -11,19 +12,31 @@ namespace ScaleMaker
         {
             InitializeComponent();
         }
-        
-        private void DrawLine(Graphics g, Pen pen, int cx, int cy, float angle, float radius_outer, float radius_inner)
-        {                        
-            double rads = (double)(angle) * 0.01745329;
-            Point inner = new Point((int)(radius_inner * Math.Sin(rads)) + cx, -(int)(radius_inner * Math.Cos(rads))+ cy);
-            Point outer = new Point((int)(radius_outer * Math.Sin(rads)) + cx, -(int)(radius_outer * Math.Cos(rads))+ cy);            
-            g.DrawLine(pen, inner, outer);            
+
+        private int W;
+        private int H;
+        private int CX;
+        private int CY;
+        private Graphics g;
+        private Bitmap bmp;
+        private string regPath = "software\\Rabbit Engineering\\ScaleMaker";
+
+
+        private void DrawLine(Color col, int linew, float angle, float radius_outer, float radius_inner)
+        {
+            using (Pen p = new Pen(col, linew))
+            {
+                double rads = (double)(angle) * 0.01745329;
+                Point inner = new Point((int)(radius_inner * Math.Sin(rads)) + CX, -(int)(radius_inner * Math.Cos(rads)) + CY);
+                Point outer = new Point((int)(radius_outer * Math.Sin(rads)) + CX, -(int)(radius_outer * Math.Cos(rads)) + CY);
+                g.DrawLine(p, inner, outer);
+            }
         }
 
-        private void DrawNumber(Graphics g, Color col, int cx, int cy, float angle, float radius, string fontname, int fontsize, string num)
+        private void DrawNumber(Color col, float angle, float radius, string fontname, int fontsize, string num)
         {
             double rads = (double)(angle) * 0.01745329;
-            Point pf = new Point((int)(radius * Math.Sin(rads)) + cx, -(int)(radius * Math.Cos(rads)) + cy);
+            Point pf = new Point((int)(radius * Math.Sin(rads)) + CX, -(int)(radius * Math.Cos(rads)) + CY);
 
             using (StringFormat sf = new StringFormat())
             {
@@ -44,36 +57,81 @@ namespace ScaleMaker
             }
         }
 
+        private string ReadRegKey(string prop)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(regPath))
+            {
+                if (key == null) return null;
+
+                return (string)key.GetValue(prop);
+            }
+        }
+        private void WriteRegKey(string prop, string value)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(regPath))
+            {
+                key.SetValue(prop, value);                
+            }
+        }
+
+        private void PrepScale(int _w, int _h)
+        {
+            if (g != null) g.Dispose();
+            if (bmp != null) bmp.Dispose();
+
+            W = _w; 
+            H = _h;
+            CX = W / 2;
+            CY = H / 2;
+            bmp = new Bitmap(W, H);
+            g = Graphics.FromImage(bmp);
+            g.Clear(Color.Transparent);
+            g.SmoothingMode = SmoothingMode.HighQuality;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             int w = 122;
             int h = 122;
-            Bitmap bmp = new Bitmap(w, h);            
-            int pw = 1;
-            Graphics g = Graphics.FromImage(bmp);
-            g.Clear(Color.Transparent);
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            using (Pen p = new Pen(Color.Red, pw))
+            PrepScale(w, h);
+            for (int i = 0; i < 12; i++)
             {
-                for (int i = 0; i < 12; i++)
-                {
-                    DrawLine(g, p, w / 2, h / 2, i*30, 58, 52); 
-                }
-                for (int i = 0; i < 12; i++)
-                {
-                    DrawLine(g, p, w / 2, h / 2, i*30+15, 58, 56);
-                }
-                for (int i = 0; i < 12; i++)
-                {
-                    DrawNumber(g, Color.Blue, w / 2, h / 2, i * 30, 43, "Arial", 10, String.Format("{0}", i * 3));
-                }
+                DrawLine(Color.Red, 2, i * 30, 58, 52);
             }
-
-            bmp.Save("g:/temp/out.png");
-            
+            for (int i = 0; i < 12; i++)
+            {
+                DrawLine(Color.Red, 2, i * 30 + 15, 58, 56);
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                DrawNumber(Color.Blue, i * 30, 43, "Arial", 10, String.Format("{0}", i * 3));
+            }
+           
             picturePreview.Image = bmp;
             picturePreview.Invalidate();
+        }
 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (g != null) g.Dispose();
+            if (bmp != null) bmp.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bmp == null) return;
+
+            if (this.saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            bmp.Save(this.saveFileDialog1.FileName);
+            WriteRegKey("pngpath", this.saveFileDialog1.FileName);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string s = ReadRegKey("pngpath");
+            if (!string.IsNullOrEmpty(s)) this.saveFileDialog1.FileName = s;
         }
     }
 }
